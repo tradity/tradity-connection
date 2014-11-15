bb449c7d2ff9370d5f16d49fedc09a008b968abe
Hauke Henningsen <sqrt@tradity.de>
2014-11-15 19:40:48 +0100

    Document connectiondata.js

--- util.js
+++ util.js
@@ -5,8 +5,39 @@ function parentPath(x) {
 	return match ? match[1] : '/';
 }
 
+function locallyUnique() {
+	locallyUnique.count = locallyUnique.count || 0;
+	return locallyUnique.count++;
+}
+
+function detectCycle(o) {
+	var seen = [];
+	
+	function dfs(o) {
+		if (o && typeof o == 'object') {
+			if (seen.indexOf(o) != -1)
+				return true;
+			
+			seen.push(o);
+			for (var key in o) {
+				if (o.hasOwnProperty(key)) {
+					var previousChain = dfs(o[key]);
+					if (previousChain)
+						return '.' + key + (previousChain === true ? '' : previousChain);
+				}
+			}
+		}
+		
+		return false;
+	}
+	
+	return dfs(o);
+}
+
 if (typeof exports != 'undefined' && exports) {
 	exports.parentPath = parentPath;
+	exports.locallyUnique = locallyUnique;
+	exports.detectCycle = detectCycle;
 	
 	if (typeof require != 'undefined' && require) {
 		var crypto = require('crypto');
