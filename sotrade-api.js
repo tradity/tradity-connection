2a5b795cc2fbfbf2e8acff24594c06311268c63f
Hauke Henningsen <sqrt@entless.org>
2015-07-30 16:41:41 +0200

    Add basic JSDoc-style documentation of this class

--- sotrade-api.js
+++ sotrade-api.js
@@ -4,13 +4,64 @@
 
 'use strict';
 
-// socket.io wrapper object
+/**
+ * Provides {@link module:sotrade-api~SoTradeConnection}.
+ * This module can be directly included or <code<require()</code>'d.
+ * 
+ * @public
+ * @module sotrade-api
+ */
+
+/**
+ * Wrapper object for an underlying socket.io transport.
+ * 
+ * This object provides an implementation of a stateless request-based protocol
+ * on top of the asynchronous event-based socket.io model.
+ * 
+ * It also implements the (Node.js standard) event emitter interface.
+ * 
+ * @property {function} connect  A callback that returns a socket.io connection instance
+ * @property {object} socket  The underlying socket.io connection instance
+ * @property {function} applyWrap  See {@link module:sotrade-api~SoTradeConnection#externallyCalled}
+ * @property {object} listeners  Object with event names as keys and arrays of callbacks
+ *             as values. The values of these arrays will be called with
+ *             the event data structures when an corresponding event is
+ *             received.
+ * @property {object} pendingIDs  Object with IDs of pending queries as keys and objects
+ *             containing information about that query as values. The latter have an
+ *             <code>cb</code> entry for a callback that will be triggered when the
+ *             reponse arrives and an optional <code>prefill</code> property. If
+ *             <code>prefill</code> is given, it should be an object and will be merged
+ *             into the response data structure.
+ * @property {int} id  Current ID counter, increased for each query
+ * @property {?object} lzma  Optional provider of LZMA decompression.
+ *             It should provide the <a href="https://github.com/nmrugg/LZMA-JS">LZMA-JS</a>
+ *             interface, i.e. an <code>decompress</code> method as described there.
+ * @property {function} protocolVersion  A function returning a currently supported protocol version.
+ * @property {object} q  An implementation of the <code>Q</code> library (e.g. <code>$q</code>).
+ * @property {?string} clientSoftwareVersion  An optional version identifier for this client.
+ * @property {function} logDevCheck  A function returning whether to log incoming/outgoing packets
+ * @property {function} logSrvCheck  A function returning whether to log server debugging information
+ * @property {object} keyStorage  An object providing <code>getKey</code> and <code>setKey</code>
+ *             methods for storing the session key.
+ * @property {?object} messageSigner  Optional instance of {@link module:signedmsg~SignedMessaging},
+ *             used for sending queries with administrative privileges.
+ * @property {bool} noSignByDefault  Whether not to sign queries by default if <code>messageSigner</code>
+ *             is present.
+ * @property {object} qCache  Cache of queries, indexed by the JSON representation of the input.
+ * @property {?object} serverConfig  The server config as received in an initial event.
+ * @property {int} _txPackets  Number of transmitted packets.
+ * @property {int} _rxPackets  Number of received packets.
+ * 
+ * @public
+ * @constructor module:sotrade-api~SoTradeConnection
+ */
 var SoTradeConnection = function(opt) {
 	this.connect = opt.connect;
 	this.socket = null;
 	this.applyWrap = opt.applyWrap || function(f) { f(); };
 	this.listeners = {}; // listener name -> array of callbacks
-	this.ids = {}; // numeric id -> {cb: callback for that id, prefill: object}
+	this.pendingIDs = {}; // numeric id -> {cb: callback for that id, prefill: object}
 	this.id = 0;
 	this.lzma = opt.lzma || null;
 	this.protocolVersion = function() { return 1; };
@@ -36,6 +87,15 @@ var SoTradeConnection = function(opt) {
 	this.init();
 };
 
+/**
+ * Provides a default implementation of a key storage that will be used
+ * when no other object is provided as <code>keyStorage</code>.
+ * 
+ * Returns an object where <code>.getKey()</code> will simply return
+ * whichever value was last set via </code>.setKey(k)</code>, otherwise null.
+ * 
+ * @function module:sotrade-api~SoTradeConnection.defaultKeyStorage
+ */
 SoTradeConnection.defaultKeyStorage = function() {
 	var key = null;
 	
@@ -50,8 +110,22 @@ SoTradeConnection.defaultKeyStorage = function() {
 	};
 };
 
+/**
+ * Return a function which is identical in behaviour to the input function,
+ * but which, when called, will be wrapped in whatever was passed as
+ * <code>applyWrap</code> to this object’s constructor.
+ * Additionally, <code>this</code> will refer to this object in the returned
+ * function.
+ * 
+ * @param {function} fn  Any function.
+ * 
+ * @returns {function}  A function with identical parameters and return type.
+ * 
+ * @function module:sotrade-api~SoTradeConnection#externallyCalled
+ */
 SoTradeConnection.prototype.externallyCalled = function(fn) {
 	var self = this;
+	
 	return function() {
 		var args = arguments;
 		
@@ -61,11 +135,21 @@ SoTradeConnection.prototype.externallyCalled = function(fn) {
 	};
 };
 
+/**
+ * If debug mode is enabled, log any arguments to <code>console</code>.
+ * 
+ * @function module:sotrade-api~SoTradeConnection#datalog
+ */
 SoTradeConnection.prototype.datalog = function() {
 	if (this.logDevCheck())
 		return console.log.apply(console, arguments);
 };
 
+/**
+ * Sets up the underlying socket.io connection and listeners on that socket.
+ * 
+ * @function module:sotrade-api~SoTradeConnection#init
+ */
 SoTradeConnection.prototype.init = function() {
 	this.socket = this.connect();
 	
@@ -115,29 +199,63 @@ SoTradeConnection.prototype.init = function() {
 	}).bind(this));
 };
 
+/**
+ * Return the underlying socket.io connection.
+ * 
+ * @function module:sotrade-api~SoTradeConnection#raw
+ */
 SoTradeConnection.prototype.raw = function() {
 	return this.socket;
 };
 
+/**
+ * Marks all pending queries as possibly unanswered.
+ * For example, this can be called in case of a server-side error
+ * which may result in a query not returning an answer.
+ * 
+ * @function module:sotrade-api~SoTradeConnection#resetExpectedResponses
+ */
 SoTradeConnection.prototype.resetExpectedResponses = function() {
-	for (var i in this.ids)
-		if (this.ids[i])
-			this.ids[i]._expect_no_response = true;
+	for (var i in this.pendingIDs)
+		if (this.pendingIDs[i])
+			this.pendingIDs[i]._expect_no_response = true;
 };
 
+/**
+ * Forces a reconnect of the underlying socket.
+ * 
+ * @function module:sotrade-api~SoTradeConnection#reconnect
+ */
 SoTradeConnection.prototype.reconnect = function() {
 	this.resetExpectedResponses();
 	
 	this.socket.connect(null, 'forceNew');
 };
 
+/**
+ * Returns true if there any pending queries, i.e. entries in
+ * <code>this.pendingIDs</code> which are not marked as not expecting a response.
+ * 
+ * @function module:sotrade-api~SoTradeConnection#hasOpenQueries
+ */
 SoTradeConnection.prototype.hasOpenQueries = function() {
-	for (var i in this.ids)
-		if (this.ids[i] && !this.ids[i]._expect_no_response)
+	for (var i in this.pendingIDs)
+		if (this.pendingIDs[i] && !this.pendingIDs[i]._expect_no_response)
 			return true;
 	return false;
 };
 
+/**
+ * Invoke all listeners for a given set of incoming data.
+ * This includes all listeners which listen on any event (<code>'*'</code>),
+ * which listen for this kind of event/response, and which wait for this
+ * specific response.
+ * 
+ * @param {object} data  The incoming data object. Here, only <code>data.type</code> is used.
+ * @param {?object} waitentry  An entry in <code>this.pendingIDs</code>, for example.
+ * 
+ * @function module:sotrade-api~SoTradeConnection#invokeListeners
+ */
 SoTradeConnection.prototype.invokeListeners = function(data, waitentry) {
 	var listener = (waitentry && waitentry.cb) || function() {};
 	
@@ -157,6 +275,14 @@ SoTradeConnection.prototype.invokeListeners = function(data, waitentry) {
 		waitentry.deferred.resolve(data);
 };
 
+/**
+ * Handle incoming data.
+ * This includes saving a received session key (e.g. after login), merging in any
+ * pre-filled data from the original query, adding timing information and
+ * invoking any listeners for the event.
+ * 
+ * @function module:sotrade-api~SoTradeConnection#responseHandler
+ */
 SoTradeConnection.prototype.responseHandler = function(data) {
 	var rid = data['is-reply-to'].split('--');
 	var type = rid[0];
@@ -166,7 +292,7 @@ SoTradeConnection.prototype.responseHandler = function(data) {
 	data.type = type;
 		
 	var numericID = parseInt(rid[1]);
-	var waitentry = this.ids[numericID];
+	var waitentry = this.pendingIDs[numericID];
 	
 	if (waitentry) {
 		for (var i in waitentry.prefill) 
@@ -187,11 +313,22 @@ SoTradeConnection.prototype.responseHandler = function(data) {
 	
 	this.datalog('<', data);
 	
-	delete this.ids[numericID];
+	delete this.pendingIDs[numericID];
 	
 	this.invokeListeners(data, waitentry);
 };
 
+/**
+ * Send a request to the server.
+ * 
+ * @param {string} evname  The name of the request type.
+ * @param {object} [data]  Additional request payload (depending on the type).
+ * @param {function} [cb]  An optional callback to be invoked when the response is received.
+ * 
+ * @returns {object} If a promise implementation is present, returns a Q-style promise.
+ * 
+ * @function module:sotrade-api~SoTradeConnection#emit
+ */
 SoTradeConnection.prototype.emit = function(evname, data, cb) {
 	if (typeof data == 'function') {
 		cb = data;
@@ -261,7 +398,7 @@ SoTradeConnection.prototype.emit = function(evname, data, cb) {
 	
 	var deferred = this.q ? this.q.defer() : null;
 	
-	this.ids[id] = {
+	this.pendingIDs[id] = {
 		cb: cb,
 		deferred: deferred,
 		prefill: prefill,
@@ -295,10 +432,20 @@ SoTradeConnection.prototype.emit = function(evname, data, cb) {
 	return deferred ? deferred.promise : null;
 };
 
+/**
+ * Return the current session key.
+ * 
+ * @function module:sotrade-api~SoTradeConnection#getKey
+ */
 SoTradeConnection.prototype.getKey = function() {
 	return this.keyStorage.getKey();
 };
 
+/**
+ * Set the current session key and, if appropriate, clear the cache.
+ * 
+ * @function module:sotrade-api~SoTradeConnection#setKey
+ */
 SoTradeConnection.prototype.setKey = function(k) {
 	this.datalog('#', 'key = ' + k);
 	
@@ -309,6 +456,15 @@ SoTradeConnection.prototype.setKey = function(k) {
 	return this.keyStorage.setKey(k);
 };
 
+/**
+ * Behaves like {@link module:sotrade-api~SoTradeConnection#on},
+ * except that after the event was triggered once, the listener is removed.
+ * Also, <code>cb</code> is optional, since a promise can be used instead.
+ * 
+ * @returns {object} If a promise implementation is present, returns a Q-style promise.
+ * 
+ * @function module:sotrade-api~SoTradeConnection#once
+ */
 SoTradeConnection.prototype.once = function(evname, cb) {
 	cb = cb || function() {};
 	
@@ -336,6 +492,18 @@ SoTradeConnection.prototype.once = function(evname, cb) {
 	return deferred.promise;
 };
 
+/**
+ * Listens on a event/response type.
+ * 
+ * @param {string} evname  A valid event or response type or <code>'*'</code>
+ * @param {function} cb  A callback that will be invoked when this event is triggered,
+ *             with the event/response payload as a parameter.
+ * @param {object} [angularScope]  An event emitter exposing the <code>'$destroy'</code>
+ *             or <code>'destroy'</code> event. When triggered, this listener will remove
+ *             itself.
+ * 
+ * @function module:sotrade-api~SoTradeConnection#on
+ */
 SoTradeConnection.prototype.on = function(evname, cb, angularScope) {
 	var index = (this.listeners[evname] = (this.listeners[evname] || [])).push(cb) - 1;
 	
@@ -355,6 +523,16 @@ SoTradeConnection.prototype.on = function(evname, cb, angularScope) {
 	}
 };
 
+/**
+ * Processes a raw server response, esp. decompresses it if encoded.
+ * Also, some fields for information on performance are added
+ * (time of receival, decode time, encoded/decoded size).
+ * 
+ * @param {object} data  A raw server response.
+ * @param {function} cb  A function that will be invoked with the decoded response.
+ * 
+ * @function module:sotrade-api~SoTradeConnection#unwrap
+ */
 SoTradeConnection.prototype.unwrap = function(data, cb) {
 	var recvTime = new Date().getTime();
 	(this.lzma && data.e == 'lzma' ? function(cont) {
@@ -377,7 +555,18 @@ SoTradeConnection.prototype.unwrap = function(data, cb) {
 	});
 };
 
+/**
+ * Returns the number of transmitted packets.
+ * 
+ * @function module:sotrade-api~SoTradeConnection#txPackets
+ */
 SoTradeConnection.prototype.txPackets = function() { return this._txPackets; };
+
+/**
+ * Returns the number of received packets.
+ * 
+ * @function module:sotrade-api~SoTradeConnection#rxPackets
+ */
 SoTradeConnection.prototype.rxPackets = function() { return this._rxPackets; };
 
 if (typeof exports != 'undefined' && exports)
